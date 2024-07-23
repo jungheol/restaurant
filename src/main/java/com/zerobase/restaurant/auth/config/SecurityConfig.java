d4c29c7de4ef8c4cb297d95ac89fb758ffdc9d58
jungheol <jungheol@naver.com>
2024-07-23 23:39:25 +0900

    SecurityConfig 파일 생성

---
+++ src/main/java/com/zerobase/restaurant/auth/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.zerobase.restaurant.auth.config;
+
+import com.zerobase.restaurant.auth.security.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationFilter authenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        sessionManagement -> sessionManagement
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(
+                        authorizeRequests -> authorizeRequests.requestMatchers("/auth/**", "swagger-ui/**").permitAll()
+                                .requestMatchers("/customer/**").hasRole("CUSTOMER")
+                                .requestMatchers("/partner/**").hasRole("PARTNER")
+                )
+                .addFilterBefore(this.authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
