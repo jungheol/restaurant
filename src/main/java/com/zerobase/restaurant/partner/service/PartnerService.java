b0f59cf3ad30c4a0def394b7249f2fe90e9196ec
jungheol <jungheol@naver.com>
2024-07-24 00:37:55 +0900

    Partner service 인터페이스 생성

---
+++ src/main/java/com/zerobase/restaurant/partner/service/PartnerService.java
@@ -0,0 +1,11 @@
+package com.zerobase.restaurant.partner.service;
+
+import com.zerobase.restaurant.partner.dto.PartnerDto;
+import com.zerobase.restaurant.partner.dto.RegisterPartner;
+
+public interface PartnerService {
+
+    PartnerDto register(RegisterPartner user);
+
+    PartnerDto partnerDetail(Long userId);
+}
