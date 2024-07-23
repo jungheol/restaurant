cf91fb8fdd1d10ffc87ab0d4cb60166e8dac4335
jungheol <jungheol@naver.com>
2024-07-24 00:36:34 +0900

    Partner repository 생성

---
+++ src/main/java/com/zerobase/restaurant/partner/repository/PartnerRepository.java
@@ -0,0 +1,8 @@
+package com.zerobase.restaurant.partner.repository;
+
+import com.zerobase.restaurant.partner.domain.Partner;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartnerRepository extends JpaRepository<Partner, Long> {
+
+}
