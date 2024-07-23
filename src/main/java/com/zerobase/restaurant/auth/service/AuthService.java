8367dfdc4072bb899895b2256f25c8a749b55b9b
jungheol <jungheol@naver.com>
2024-07-23 23:39:15 +0900

    AuthService, MemberType 파일 생성

---
+++ src/main/java/com/zerobase/restaurant/auth/service/AuthService.java
@@ -0,0 +1,19 @@
+package com.zerobase.restaurant.auth.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class AuthService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
---
+++ src/main/java/com/zerobase/restaurant/auth/type/MemberType.java
@@ -0,0 +1,13 @@
+package com.zerobase.restaurant.auth.type;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MemberType {
+    CUSTOMER("ROLE_CUSTOMER"),
+    PARTNER("ROLE_PARTNER");
+
+    private final String description;
+}
