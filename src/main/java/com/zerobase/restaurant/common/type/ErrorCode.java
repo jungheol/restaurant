278710502636602120f67326c2810230003732ff
jungheol <jungheol@naver.com>
2024-07-23 18:28:57 +0900

    ErrorCode 추가

--- src/main/java/com/zerobase/restaurant/common/type/ErrorCode.java
+++ src/main/java/com/zerobase/restaurant/common/type/ErrorCode.java
@@ -7,7 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
     INTERNAL_SERVER_ERROR("내부 서버 오류가 발생했습니다."),
-    INVALID_REQUEST("잘못된 요청입니다.")
+    INVALID_REQUEST("잘못된 요청입니다."),
+    USER_NOT_FOUND("사용자를 찾을 수 없습니다."),
+    PASSWORD_NOT_MATCHED("비밀번호가 일치하지 않습니다."),
+    ALREADY_EXISTED_RESTAURANT("이미 사용중인 매장 이름입니다."),
+    ALREADY_EXISTED_USER("이미 가입된 회원명입니다.")
     ;
 
     private final String description;
