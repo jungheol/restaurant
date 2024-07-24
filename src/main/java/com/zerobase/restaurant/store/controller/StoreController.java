502a646b83dc7b5967e6858949d790ceadb267c5
jungheol <jungheol@naver.com>
2024-07-24 16:56:02 +0900

    store controller 구현(register, delete)

---
+++ src/main/java/com/zerobase/restaurant/store/controller/StoreController.java
@@ -0,0 +1,34 @@
+package com.zerobase.restaurant.store.controller;
+
+import com.zerobase.restaurant.store.dto.RegisterStore;
+import com.zerobase.restaurant.store.service.StoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/store")
+@RequiredArgsConstructor
+public class StoreController {
+
+    private final StoreService storeService;
+
+    @PostMapping("/partner/register")
+    @PreAuthorize("hasRole('PARTNER')")
+    public RegisterStore.Response registerStore(
+            @RequestBody RegisterStore.Request request
+    ) {
+        return RegisterStore.Response.from(this.storeService.registerStore(request));
+    }
+
+    @DeleteMapping("/partner/delete")
+    @PreAuthorize("hasRole('PARTNER')")
+    public ResponseEntity<?> deleteStore(
+            @RequestParam("id") Long partnerId,
+            @RequestParam("store") Long storeId
+    ) {
+        this.storeService.deleteStore(storeId, partnerId);
+        return ResponseEntity.ok("매장 정보가 삭제되었습니다.");
+    }
+}
