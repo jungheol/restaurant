53256e8eab31a6f9a697b9a606cff6f8177cd2fc
jungheol <jungheol@naver.com>
2024-07-24 14:49:04 +0900

    partner controller 추가

---
+++ src/main/java/com/zerobase/restaurant/partner/controller/PartnerController.java
@@ -0,0 +1,29 @@
+package com.zerobase.restaurant.partner.controller;
+
+import com.zerobase.restaurant.partner.dto.RegisterPartner;
+import com.zerobase.restaurant.partner.service.PartnerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class PartnerController {
+
+    private final PartnerService partnerService;
+
+    @PostMapping("/register/partner")
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterPartner request) {
+        return ResponseEntity.ok().body(
+                request.from(this.partnerService.register(request))
+        );
+    }
+
+    @GetMapping("/partner/info")
+    public ResponseEntity<?> getPartnerInfo(@RequestParam("id") @Valid Long id) {
+        return ResponseEntity.ok(this.partnerService.partnerDetail(id));
+    }
+
+
+}
