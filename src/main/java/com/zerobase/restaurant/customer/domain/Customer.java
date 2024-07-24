a55de01d2aab2e9cc43d29588e00888bc13043de
jungheol <jungheol@naver.com>
2024-07-24 22:29:37 +0900

    customer domain 생성

---
+++ src/main/java/com/zerobase/restaurant/customer/domain/Customer.java
@@ -0,0 +1,44 @@
+package com.zerobase.restaurant.customer.domain;
+
+import com.zerobase.restaurant.auth.type.MemberType;
+import com.zerobase.restaurant.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer extends BaseTimeEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Enumerated(EnumType.STRING)
+    private MemberType memberType;
+
+    @NotBlank
+    private String password;
+
+    @NotBlank
+    private String phoneNumber;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_CUSTOMER"));
+    }
+}
