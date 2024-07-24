fe8e21d1b16b2a9b0f1f0a26b6c85bc7fdb003ec
jungheol <jungheol@naver.com>
2024-07-24 15:38:58 +0900

    AuthService 생성

--- src/main/java/com/zerobase/restaurant/auth/service/AuthService.java
+++ src/main/java/com/zerobase/restaurant/auth/service/AuthService.java
@@ -1,19 +1,59 @@
 package com.zerobase.restaurant.auth.service;
 
+import com.zerobase.restaurant.auth.dto.Login;
+import com.zerobase.restaurant.auth.type.MemberType;
+import com.zerobase.restaurant.common.exception.CustomException;
+import com.zerobase.restaurant.partner.domain.Partner;
+import com.zerobase.restaurant.partner.repository.PartnerRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import static com.zerobase.restaurant.common.type.ErrorCode.PARTNER_NOT_FOUND;
+import static com.zerobase.restaurant.common.type.ErrorCode.PASSWORD_NOT_MATCHED;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class AuthService implements UserDetailsService {
 
+    private final PartnerRepository partnerRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Partner authenticatePartner(Login login) {
+        Partner partner = checkPartnerName(login.getUsername());
+
+        if (!this.passwordEncoder.matches(login.getPassword(), partner.getPassword())) {
+            throw new CustomException(PASSWORD_NOT_MATCHED);
+        }
+
+        return partner;
+    }
+
+    private Partner checkPartnerName(String username) {
+        return this.partnerRepository.findByUsername(username)
+                .orElseThrow(() -> new CustomException(PARTNER_NOT_FOUND));
+    }
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws CustomException {
+        if (this.partnerRepository.existsByUsername(username)) {
+            Partner partner = checkPartnerName(username);
+
+            return createUserDetail(partner.getUsername(),
+                    partner.getPassword(), MemberType.PARTNER);
+        }
+
+        throw new CustomException(PARTNER_NOT_FOUND);
+    }
+
+    private UserDetails createUserDetail(String username, String password, MemberType memberType) {
+        return User.withUsername(username)
+                .password(this.passwordEncoder.encode(password))
+                .roles(String.valueOf(memberType))
+                .build();
     }
 }
--- src/main/java/com/zerobase/restaurant/partner/repository/PartnerRepository.java
+++ src/main/java/com/zerobase/restaurant/partner/repository/PartnerRepository.java
@@ -3,6 +3,9 @@ package com.zerobase.restaurant.partner.repository;
 import com.zerobase.restaurant.partner.domain.Partner;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PartnerRepository extends JpaRepository<Partner, Long> {
     boolean existsByUsername(String username);
+    Optional<Partner> findByUsername(String username);
 }
