82affe567758cd68b268f55e22c18c662072b27a
jungheol <jungheol@naver.com>
2024-07-24 14:49:49 +0900

    register partner 수정

--- src/main/java/com/zerobase/restaurant/partner/dto/RegisterPartner.java
+++ src/main/java/com/zerobase/restaurant/partner/dto/RegisterPartner.java
@@ -3,10 +3,12 @@ package com.zerobase.restaurant.partner.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class RegisterPartner {
     private String username;
     private String password;
