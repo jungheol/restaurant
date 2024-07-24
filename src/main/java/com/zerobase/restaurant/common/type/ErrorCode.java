409c534789f48ebd4e7cea20169fca614efa97ce
jungheol <jungheol@naver.com>
2024-07-24 16:55:35 +0900

    store service에서 필요한 error code 추가

--- src/main/java/com/zerobase/restaurant/common/type/ErrorCode.java
+++ src/main/java/com/zerobase/restaurant/common/type/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
     INVALID_REQUEST("잘못된 요청입니다."),
     USER_NOT_FOUND("사용자를 찾을 수 없습니다."),
     PARTNER_NOT_FOUND("파트너를 찾을 수 없습니다."),
+    STORE_NOT_FOUND("매장을 찾을 수 없습니다."),
+    STORE_NOT_MATCH_MANAGER("파트너 본인의 매장이 아닙니다."),
     PASSWORD_NOT_MATCHED("비밀번호가 일치하지 않습니다."),
     ALREADY_EXISTED_RESTAURANT("이미 사용중인 매장 이름입니다."),
     ALREADY_EXISTED_USER("이미 가입된 회원명입니다.")
