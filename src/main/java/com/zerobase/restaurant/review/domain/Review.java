a1e376fe75648fd542476a6b8e6da5d7f32954a0
jungheol <jungheol@naver.com>
2024-07-30 15:04:14 +0900

    review domain 생성

---
+++ src/main/java/com/zerobase/restaurant/review/domain/Review.java
@@ -0,0 +1,37 @@
+package com.zerobase.restaurant.review.domain;
+
+import com.zerobase.restaurant.common.domain.BaseTimeEntity;
+import com.zerobase.restaurant.customer.domain.Customer;
+import com.zerobase.restaurant.reservation.domain.Reservation;
+import com.zerobase.restaurant.store.domain.Store;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String reviewContent;
+
+    private Integer rating;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @ManyToOne
+    @JoinColumn(name = "reservation_id")
+    private Reservation reservation;
+}
